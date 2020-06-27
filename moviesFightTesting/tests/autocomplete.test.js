3cb562ac2232806c590b0057cf026488481a2900
Henrique S.A <henriqueator@gmail.com>
2020-06-27 18:37:35 -0300

    Web-browser testing using mocha
    - added Chai assert to test dropdown

---
+++ moviesFightTesting/tests/autocomplete.test.js
@@ -0,0 +1,13 @@
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData(movies) {
+      return [{ Title: 'Avengers' }, { Title: 'Titanic' }];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).not.to.include('is-active');
+});
